1f766206ef27cf4f290cc066d4971e9fcb91e59f
nemodelivers1001 <nemodelivers1001@gmail.com>
2025-10-12 23:10:20 +0500

    Updated app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import { createBalloons, createCakeAnimation, createWishesMessage } from './celebration.js';
 
-const TARGET_TIMESTAMP = '2025-10-12T23:03:00+05:00';
+const TARGET_TIMESTAMP = '2025-10-12T23:11:00+05:00';
 const TARGET_DATE = new Date(TARGET_TIMESTAMP);
 
 console.log('=== Countdown Debug Info ===');
